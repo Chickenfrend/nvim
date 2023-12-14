4db8b9fcdd4ff04d6c3560b96e64b73821229d09
Benjamin Lain <ben@localhost.localdomain>
2023-12-13 17:38:25 -0800

    Added vim stuff

---
+++ lua/plugins/init.lua
@@ -0,0 +1,7 @@
+return {
+  {"ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {"nvim-treesitter/playground"},
+  {"mbbill/undotree"},
+  {"tpope/vim-fugitive"},
+}
